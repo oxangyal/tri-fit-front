3b45d9145273546e3cbcceef5c3ec1417ef09361
oxangyal <Oxana.mitch@gmail.com>
2024-01-29 15:05:34 -0500

    Updated navbar burger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
+
+import BurgerMenuIcon from "../assets/burgermenu.png";
+import CloseIcon from "../assets/closeburger.png";
 import { Link } from "react-router-dom";
 import Logo from "../assets/triblue.png";
-import BurgerMenuIcon from "../assets/burgermenu.png"; // Import your custom burger menu icon
-import CloseIcon from "../assets/closeburger.png"; // Import your custom close icon
 
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -21,7 +22,7 @@ const Navbar = () => {
                             alt="Logo icon"
                             className="w-28 h-28 mr-6"
                         />
-                        <span className="text-xl font-bold">Tri Fit</span>
+                        <span className="text-2xl font-bold">Tri Fit</span>
                     </div>
                     <div className="lg:hidden">
                         {/* Show only on small screens */}
@@ -50,38 +51,44 @@ const Navbar = () => {
                             menuOpen ? "flex flex-col" : "hidden"
                         }`}
                     >
-                        {/* Show on large screens and mobile menu */}
+                        {/* Show on large screens and mobile */}
+                        <Link
+                            to="/"
+                            className="text-white text-2xl px-4 py-2 rounded bg-151c1e hover:bg-gray-800 transition duration-300"
+                        >
+                            Home
+                        </Link>
                         <Link
                             to="/about"
-                            className="text-white text-lg px-4 py-2 rounded bg-151c1e hover:bg-gray-800 transition duration-300"
+                            className="text-white text-2xl px-4 py-2 rounded bg-151c1e hover:bg-gray-800 transition duration-300"
                         >
                             About
                         </Link>
                         <Link
-                            to="/workouts"
-                            className="text-white text-lg px-4 py-2 rounded bg-151c1e hover:bg-gray-800 transition duration-300"
+                            to="/workout"
+                            className="text-white text-2xl px-4 py-2 rounded bg-151c1e hover:bg-gray-800 transition duration-300"
                         >
                             Workouts
                         </Link>
-                        <Link
+                        {/* <Link
                             to="/register"
-                            className={`text-white text-lg px-4 py-2 rounded ${
+                            className={`text-white text-2xl px-4 py-2 rounded ${
                                 menuOpen
                                     ? "bg-custom-color"
                                     : "bg-blue-600 hover:bg-blue-500"
                             } transition duration-300`}
                         >
                             Signup
-                        </Link>
+                        </Link> */}
                         <Link
-                            to="/login"
-                            className={`text-white text-lg px-4 py-2 rounded ${
+                            to="/auth"
+                            className={`text-white text-2xl px-4 py-2 font-bold rounded ${
                                 menuOpen
                                     ? "bg-custom-color"
-                                    : "bg-blue-600 hover:bg-blue-500"
+                                    : "bg-blue-500 hover:bg-blue-500"
                             } transition duration-300`}
                         >
-                            Login
+                            Signup | Login
                         </Link>
                     </div>
                 </div>
