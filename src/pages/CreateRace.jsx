7b6b16d6fdbe302feee696a87a59e6d3d506e081
oxangyal <Oxana.mitch@gmail.com>
2024-02-02 23:47:47 -0500

    Create workout and Create race forms implemented with backend, formik validation

---
+++ src/pages/CreateRace.jsx
@@ -0,0 +1,317 @@
+import * as Yup from "yup";
+
+import { ToastContainer, toast } from "react-toastify";
+
+import React from "react";
+import axios from "axios";
+import { useFormik } from "formik";
+import { useNavigate } from "react-router-dom";
+
+const CreateRace = () => {
+    const navigate = useNavigate();
+
+    const formik = useFormik({
+        initialValues: {
+            race: "",
+            title: "",
+            timeOfCompletion: {
+                hours: 0,
+                minutes: 0,
+            },
+            date: "",
+            location: {
+                city: "",
+                state: "",
+            },
+        },
+        validationSchema: Yup.object({
+            race: Yup.string().required("Race is required"),
+            title: Yup.string().required("Title is required"),
+            timeOfCompletion: Yup.object().shape({
+                hours: Yup.number().min(0),
+                minutes: Yup.number()
+                    .min(0)
+                    .max(59),
+            }),
+            date: Yup.date().required("Date is required"),
+            location: Yup.object().shape({
+                city: Yup.string(),
+                state: Yup.string(),
+            }),
+        }),
+        onSubmit: async (values) => {
+            try {
+                const jwtToken = localStorage.getItem("jwtToken");
+                const response = await axios.post(
+                    `${process.env.REACT_APP_BASE_URL}/api/v1/races`,
+                    values,
+                    {
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: `Bearer ${jwtToken}`,
+                        },
+                    }
+                );
+
+                console.log(response.data.race);
+                toast.success("Your race was successfully created!", {
+                    position: "top-center",
+                    autoClose: 3000,
+                    hideProgressBar: true,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                });
+                await new Promise((resolve) => setTimeout(resolve, 1500));
+
+                navigate("/races");
+            } catch (error) {
+                toast.error("Error creating race. Please try again.", {
+                    position: "top-center",
+                    autoClose: 3000,
+                    hideProgressBar: true,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                });
+                console.error("Error creating race:", error);
+            }
+        },
+    });
+
+    return (
+        <div className="bg-gradient-to-b from-custom-color to-blue-500 min-h-screen flex items-start justify-center">
+            <div className="bg-gradient-to-t from-custom-color to-blue-500 p-8 rounded-xl shadow-md max-w-md w-full">
+                <h2 className="text-2xl font-bold text-white mb-8 text-center font-nunito">
+                    Create Race
+                </h2>
+                <form onSubmit={formik.handleSubmit}>
+                    <div className="mb-4">
+                        <label
+                            className="block font-bold text-white mb-2 text-xl font-nunito"
+                            htmlFor="race"
+                        >
+                            Race
+                        </label>
+                        <select
+                            name="race"
+                            id="race"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.race}
+                            className="form-input pl-3 w-full h-10 mb-5 rounded-md"
+                            required
+                        >
+                            <option value="" disabled>
+                                Select Race
+                            </option>
+                            <option value="sprint">Sprint</option>
+                            <option value="olympic">Olympic</option>
+                            <option value="half">Half</option>
+                            <option value="full">Full</option>
+                        </select>
+                        {formik.touched.race && formik.errors.race && (
+                            <p className="text-red-500 text-xs mt-1">
+                                {formik.errors.race}
+                            </p>
+                        )}
+                    </div>
+
+                    <div className="mb-4">
+                        <label
+                            className="block font-bold text-white mb-2 text-xl font-nunito"
+                            htmlFor="title"
+                        >
+                            Title
+                        </label>
+                        <input
+                            type="text"
+                            name="title"
+                            id="title"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.title}
+                            className={`form-input w-full pl-3 h-10 mb-5 rounded-md ${
+                                formik.touched.title && formik.errors.title
+                                    ? "border-red-500"
+                                    : ""
+                            }`}
+                            required
+                        />
+                        {formik.touched.title && formik.errors.title && (
+                            <p className="text-red-500 text-xs mt-1">
+                                {formik.errors.title}
+                            </p>
+                        )}
+                    </div>
+
+                    <div className="mb-4 flex">
+                        <div className="mr-4">
+                            <label
+                                className="block font-bold text-white mb-2 text-xl font-nunito"
+                                htmlFor="timeOfCompletion.hours"
+                            >
+                                Hours
+                            </label>
+                            <input
+                                type="number"
+                                name="timeOfCompletion.hours"
+                                id="timeOfCompletion.hours"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.timeOfCompletion.hours}
+                                className={`form-input w-full pl-3 h-10 mb-5 rounded-md ${
+                                    formik.touched.timeOfCompletion?.hours &&
+                                    formik.errors.timeOfCompletion?.hours
+                                        ? "border-red-500"
+                                        : ""
+                                }`}
+                                required
+                            />
+                            {formik.touched.timeOfCompletion?.hours &&
+                                formik.errors.timeOfCompletion?.hours && (
+                                    <p className="text-red-500 text-xs mt-1">
+                                        {formik.errors.timeOfCompletion.hours}
+                                    </p>
+                                )}
+                        </div>
+                        <div>
+                            <label
+                                className="block font-bold text-white mb-2 text-xl font-nunito"
+                                htmlFor="timeOfCompletion.minutes"
+                            >
+                                Minutes
+                            </label>
+                            <input
+                                type="number"
+                                name="timeOfCompletion.minutes"
+                                id="timeOfCompletion.minutes"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.timeOfCompletion.minutes}
+                                className={`form-input w-full pl-3 h-10 mb-5 rounded-md ${
+                                    formik.touched.timeOfCompletion?.minutes &&
+                                    formik.errors.timeOfCompletion?.minutes
+                                        ? "border-red-500"
+                                        : ""
+                                }`}
+                                required
+                            />
+                            {formik.touched.timeOfCompletion?.minutes &&
+                                formik.errors.timeOfCompletion?.minutes && (
+                                    <p className="text-red-500 text-xs mt-1">
+                                        {formik.errors.timeOfCompletion.minutes}
+                                    </p>
+                                )}
+                        </div>
+                    </div>
+
+                    <div className="mb-4 flex">
+                        <div className="mr-4">
+                            <label
+                                className="block font-bold text-white mb-2 text-xl font-nunito"
+                                htmlFor="location.city"
+                            >
+                                City
+                            </label>
+                            <input
+                                type="text"
+                                name="location.city"
+                                id="location.city"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.location.city}
+                                className={`form-input w-full pl-3 h-10 mb-5 rounded-md ${
+                                    formik.touched.location?.city &&
+                                    formik.errors.location?.city
+                                        ? "border-red-500"
+                                        : ""
+                                }`}
+                                required
+                            />
+                            {formik.touched.location?.city &&
+                                formik.errors.location?.city && (
+                                    <p className="text-red-500 text-xs mt-1">
+                                        {formik.errors.location.city}
+                                    </p>
+                                )}
+                        </div>
+                        <div>
+                            <label
+                                className="block font-bold text-white mb-2 text-xl font-nunito"
+                                htmlFor="location.state"
+                            >
+                                State
+                            </label>
+                            <input
+                                type="text"
+                                name="location.state"
+                                id="location.state"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.location.state}
+                                className={`form-input w-full pl-3 h-10 mb-5 rounded-md ${
+                                    formik.touched.location?.state &&
+                                    formik.errors.location?.state
+                                        ? "border-red-500"
+                                        : ""
+                                }`}
+                                required
+                            />
+                            {formik.touched.location?.state &&
+                                formik.errors.location?.state && (
+                                    <p className="text-red-500 text-xs mt-1">
+                                        {formik.errors.location.state}
+                                    </p>
+                                )}
+                        </div>
+                    </div>
+
+                    <div className="mb-4">
+                        <label
+                            className="block font-bold text-white mb-2 text-xl font-nunito"
+                            htmlFor="date"
+                        >
+                            Date
+                        </label>
+                        <input
+                            type="date"
+                            name="date"
+                            id="date"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.date}
+                            className="form-input w-full pl-3 pr-3 h-10 mb-5 rounded-md"
+                            required
+                        />
+                        {formik.touched.date && formik.errors.date && (
+                            <p className="text-red-500 text-xs mt-1">
+                                {formik.errors.date}
+                            </p>
+                        )}
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="bg-blue-500 text-white py-4 px-10 rounded-md font-nunito hover:opacity-55 mt-10 text-xl"
+                    >
+                        Create
+                    </button>
+                </form>
+                <ToastContainer
+                    position="top-center"
+                    autoClose={3000}
+                    hideProgressBar
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                />
+            </div>
+        </div>
+    );
+};
+
+export default CreateRace;
---
+++ src/pages/CreateWorkout.jsx
@@ -0,0 +1,285 @@
+import * as Yup from "yup";
+
+import { ToastContainer, toast } from "react-toastify";
+
+import React from "react";
+import axios from "axios";
+import { useFormik } from "formik";
+import { useNavigate } from "react-router-dom";
+
+const CreateWorkout = () => {
+    const navigate = useNavigate();
+
+    const formik = useFormik({
+        initialValues: {
+            workoutType: "",
+            duration: "",
+            intensity: "",
+            indoor: false,
+            outdoor: false,
+            date: "",
+            description: "",
+        },
+        validationSchema: Yup.object({
+            workoutType: Yup.string().required("Workout type is required"),
+            duration: Yup.number()
+                .required("Duration is required")
+                .positive("Duration must be a positive number")
+                .integer("Duration must be a whole number"),
+            intensity: Yup.string(),
+            indoor: Yup.boolean(),
+            outdoor: Yup.boolean(),
+            date: Yup.date()
+                .min(new Date(), "Date cannot be in the past")
+                .required("Date is required"),
+            description: Yup.string(),
+        }),
+        onSubmit: async (values) => {
+            try {
+                const jwtToken = localStorage.getItem("jwtToken");
+                const response = await axios.post(
+                    `${process.env.REACT_APP_BASE_URL}/api/v1/workouts`,
+                    values,
+                    {
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: `Bearer ${jwtToken}`,
+                        },
+                    }
+                );
+
+                console.log(response.data.workout);
+                // const createdWorkout = response.data.workout;
+                toast.success("Your workout was successfully created!", {
+                    position: "top-center",
+                    autoClose: 3000,
+                    hideProgressBar: true,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                });
+                await new Promise((resolve) => setTimeout(resolve, 1500));
+
+                navigate("/workouts");
+            } catch (error) {
+                toast.error("Error creating workout. Please try again.", {
+                    position: "top-center",
+                    autoClose: 3000,
+                    hideProgressBar: true,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                });
+                console.error("Error creating workout:", error);
+            }
+        },
+    });
+
+    return (
+        <div className="bg-gradient-to-b from-custom-color to-blue-500 min-h-screen flex items-start justify-center">
+            <div className="bg-gradient-to-t from-custom-color to-blue-500 p-8 rounded-xl shadow-md max-w-md w-full">
+                <h2 className="text-2xl font-bold text-white mb-8 text-center font-nunito">
+                    Create Workout
+                </h2>
+                <form onSubmit={formik.handleSubmit}>
+                    <div className="mb-4">
+                        <label
+                            className="block font-bold text-white mb-2 text-xl font-nunito"
+                            htmlFor="workoutType"
+                        >
+                            Workout
+                        </label>
+                        <select
+                            name="workoutType"
+                            id="workoutType"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.workoutType}
+                            className="form-input pl-3 w-full h-10 mb-5 rounded-md"
+                            required
+                        >
+                            <option value="" disabled>
+                                Select Workout
+                            </option>
+                            <option value="swim">Swim</option>
+                            <option value="run">Run</option>
+                            <option value="cycle">Cycle</option>
+                        </select>
+                        {formik.touched.workoutType &&
+                            formik.errors.workoutType && (
+                                <p className="text-red-500 text-xs mt-1">
+                                    {formik.errors.workoutType}
+                                </p>
+                            )}
+                    </div>
+                    <div className="mb-4">
+                        <label
+                            className="block font-bold text-white mb-2 text-xl font-nunito"
+                            htmlFor="duration"
+                        >
+                            Duration (minutes)
+                        </label>
+                        <input
+                            type="text"
+                            name="duration"
+                            id="duration"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.duration}
+                            className={`form-input w-full pl-3 h-10 mb-5 rounded-md ${
+                                formik.touched.duration &&
+                                formik.errors.duration
+                                    ? "border-red-500"
+                                    : ""
+                            }`}
+                            required
+                        />
+                        {formik.touched.duration && formik.errors.duration && (
+                            <p className="text-red-500 text-xs mt-1">
+                                {formik.errors.duration}
+                            </p>
+                        )}
+                    </div>
+                    <div className="mb-4">
+                        <label
+                            className="block font-bold text-white mb-2 text-xl font-nunito"
+                            htmlFor="intensity"
+                        >
+                            Intensity
+                        </label>
+                        <select
+                            name="intensity"
+                            id="intensity"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.intensity}
+                            className="form-input pl-3 w-full h-10 mb-5 pr-3 rounded-md"
+                            required
+                        >
+                            <option value="" disabled>
+                                Select Intensity
+                            </option>
+                            <option value="low">Low</option>
+                            <option value="moderate">Moderate</option>
+                            <option value="high">High</option>
+                        </select>
+                        {formik.touched.intensity &&
+                            formik.errors.intensity && (
+                                <p className="text-red-500 text-xs mt-1">
+                                    {formik.errors.intensity}
+                                </p>
+                            )}
+                    </div>
+                    <div className="mb-4">
+                        {/* <label
+                            className="block font-bold text-white mb-2 text-xl font-nunito"
+                            htmlFor="indoor"
+                        >
+                            Indoor
+                        </label> */}
+                        <input
+                            type="radio"
+                            id="indoor"
+                            name="indoor"
+                            checked={formik.values.indoor}
+                            onChange={() =>
+                                formik.setFieldValue("indoor", true)
+                            }
+                            onBlur={formik.handleBlur}
+                        />
+                        <label
+                            htmlFor="indoor"
+                            className="text-white pl-3 mr-4"
+                        >
+                            Indoor
+                        </label>
+
+                        <input
+                            type="radio"
+                            id="outdoor"
+                            name="indoor"
+                            checked={formik.values.outdoor}
+                            onChange={() =>
+                                formik.setFieldValue("outdoor", true)
+                            }
+                            onBlur={formik.handleBlur}
+                        />
+                        <label htmlFor="outdoor" className="text-white pl-3">
+                            Outdoor
+                        </label>
+                        {formik.touched.indoor && formik.errors.indoor && (
+                            <p className="text-red-500 text-xs mt-1">
+                                {formik.errors.indoor}
+                            </p>
+                        )}
+                    </div>
+                    <div className="mb-4">
+                        <label
+                            className="block font-bold text-white mb-2 text-xl font-nunito"
+                            htmlFor="date"
+                        >
+                            Date
+                        </label>
+                        <input
+                            type="date"
+                            name="date"
+                            id="date"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.date}
+                            className="form-input w-full pl-3 pr-3 h-10 mb-5 rounded-md"
+                            required
+                        />
+                        {formik.touched.date && formik.errors.date && (
+                            <p className="text-red-500 text-xs mt-1">
+                                {formik.errors.date}
+                            </p>
+                        )}
+                    </div>
+                    <div className="mb-4">
+                        <label
+                            className="block font-bold text-white mb-2 text-xl font-nunito"
+                            htmlFor="description"
+                        >
+                            Description
+                        </label>
+                        <textarea
+                            name="description"
+                            id="description"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.description}
+                            className="form-input pl-3 pt-2 w-full h-32 mb-5 rounded-md"
+                            required
+                        ></textarea>
+                        {formik.touched.description &&
+                            formik.errors.description && (
+                                <p className="text-red-500 text-xs mt-1">
+                                    {formik.errors.description}
+                                </p>
+                            )}
+                    </div>
+                    <button
+                        type="submit"
+                        className="bg-blue-500 text-white py-4 px-10 rounded-md font-nunito hover:opacity-55 mt-10 text-xl"
+                    >
+                        Create
+                    </button>
+                </form>
+                <ToastContainer
+                    position="top-center"
+                    autoClose={3000}
+                    hideProgressBar
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                />
+            </div>
+        </div>
+    );
+};
+
+export default CreateWorkout;
