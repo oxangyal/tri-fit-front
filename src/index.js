e8f1ffcb4ae166404e4c3860183f82614ef6f8ea
oxangyal <Oxana.mitch@gmail.com>
2024-02-09 11:13:01 -0500

    Corrected index.jx with context implementation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import './index.css';
 import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
+
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { UserContextProvider } from "./context/userContext";
 import reportWebVitals from './reportWebVitals';
-import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <UserContextProvider>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </UserContextProvider>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
